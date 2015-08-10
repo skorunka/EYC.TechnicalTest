a75d5f03e96382c90752e6742130b28556ce2bc7
Frantisek Skorunka <skorunka@outlook.com>
2015-08-10 16:37:56 +0200

    Tests for Null, Empty or WhiteSpace supplierId and productName parameters, when calculating retailer charge.

--- EYC.Services.Tests/UnitTests/RetailerChargeCaculationService.cs
+++ EYC.Services.Tests/UnitTests/RetailerChargeCaculationService.cs
@@ -2,12 +2,36 @@
 // ReSharper disable InconsistentNaming
 namespace EYC.Services.UnitTests
 {
+	using System;
+
 	using EYC.Services.Implementations;
 
 	using Xunit;
 
 	public class RetailerChargeCaculationServiceUnitTests
 	{
+		[Theory]
+		[InlineData(null), InlineData(""), InlineData("  "), InlineData("\n \t")]
+		public void throw_ArgumentException_when_SupplierId_is_NullOrEmptyOrWhitespace(string supplierId)
+		{
+			var productName = "Juice";
+
+			var service = new RetailerChargeCaculationService() as IRetailerChargeCaculationService;
+
+			Assert.Throws<ArgumentException>(() => service.CalculateRetailerCharge(supplierId, productName));
+		}
+
+		[Theory]
+		[InlineData(null), InlineData(""), InlineData("  "), InlineData("\n \t")]
+		public void throw_ArgumentException_when_ProductName_is_NullOrEmptyOrWhitespace(string productName)
+		{
+			var supplierId = "SupplierTwo";
+
+			var service = new RetailerChargeCaculationService() as IRetailerChargeCaculationService;
+
+			Assert.Throws<ArgumentException>(() => service.CalculateRetailerCharge(supplierId, productName));
+		}
+
 		[Fact]
 		public void return_5000_for_SupplierTwo_and_Juice()
 		{
--- EYC.Services/Implementations/RetailerChargeCaculationService.cs
+++ EYC.Services/Implementations/RetailerChargeCaculationService.cs
@@ -1,9 +1,21 @@
 ﻿namespace EYC.Services.Implementations
 {
+	using System;
+
 	public class RetailerChargeCaculationService : IRetailerChargeCaculationService
 	{
 		public decimal CalculateRetailerCharge(string supplierId, string productName)
 		{
+			if (string.IsNullOrWhiteSpace(supplierId))
+			{
+				throw new ArgumentException("SupplierId can not be null, empty or whitespace.", nameof(supplierId));
+			}
+
+			if (string.IsNullOrWhiteSpace(productName))
+			{
+				throw new ArgumentException("ProductName can not be null, empty or whitespace.", nameof(productName));
+			}
+
 			return 5000m;
 		}
 	}
