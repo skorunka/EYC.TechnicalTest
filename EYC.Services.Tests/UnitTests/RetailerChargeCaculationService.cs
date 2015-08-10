5bf8812dc2c009e79597dfa35215289a6d153b22
Frantisek Skorunka <skorunka@outlook.com>
2015-08-10 16:22:41 +0200

    Added missing files.

---
+++ EYC.Services.Tests/UnitTests/RetailerChargeCaculationService.cs
@@ -0,0 +1,24 @@
+﻿// ReSharper disable CheckNamespace
+// ReSharper disable InconsistentNaming
+namespace EYC.Services.UnitTests
+{
+	using EYC.Services.Implementations;
+
+	using Xunit;
+
+	public class RetailerChargeCaculationServiceUnitTests
+	{
+		[Fact]
+		public void return_5000_for_SupplierTwo_and_Juice()
+		{
+			var expectedResult = 5000m;
+			var supplierId = "SupplierTwo";
+			var productName = "Juice";
+
+			var service = new RetailerChargeCaculationService() as IRetailerChargeCaculationService;
+			var result = service.CalculateRetailerCharge(supplierId, productName);
+
+			Assert.Equal(expectedResult, result);
+		}
+	}
+}
